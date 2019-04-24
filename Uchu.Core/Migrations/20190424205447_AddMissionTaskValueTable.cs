75ff6b18f445ea948007de50d6516fb45e2acef3
yuwui <uwu@yui.pet>
2019-04-24 22:55:28 +0200

    update migrations

---
+++ Uchu.Core/Migrations/20190424205447_AddMissionTaskValueTable.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace Uchu.Core.Migrations
+{
+    public partial class AddMissionTaskValueTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Values",
+                table: "MissionTasks");
+
+            migrationBuilder.CreateTable(
+                name: "MissionTaskValue",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    Value = table.Column<float>(nullable: false),
+                    MissionTaskId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MissionTaskValue", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MissionTaskValue_MissionTasks_MissionTaskId",
+                        column: x => x.MissionTaskId,
+                        principalTable: "MissionTasks",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MissionTaskValue_MissionTaskId",
+                table: "MissionTaskValue",
+                column: "MissionTaskId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MissionTaskValue");
+
+            migrationBuilder.AddColumn<List<float>>(
+                name: "Values",
+                table: "MissionTasks",
+                nullable: false);
+        }
+    }
+}
