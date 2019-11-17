0f6370677c9ba85b86dda823d454f386c93c5891
wincent <wincent.holm@gmail.com>
2019-11-17 16:32:14 +0100

    Forgot these...

---
+++ Uchu.Core/Migrations/20191117150805_SessionTableCreated.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Uchu.Core.Migrations
+{
+    public partial class SessionTableCreated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SessionCaches",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Key = table.Column<string>(nullable: true),
+                    CharacterId = table.Column<long>(nullable: false),
+                    UserId = table.Column<long>(nullable: false),
+                    ZoneId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SessionCaches", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SessionCaches");
+        }
+    }
+}
