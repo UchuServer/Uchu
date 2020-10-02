3444a96527ebe2451bde8c4d094b1d3f63ff2bfe
encry <17jbradford@tqacademy.co.uk>
2020-10-02 22:23:55 +0100

    Create MonumentBirds.cs

---
+++ Uchu.StandardScripts/AvantGardens/MonumentBirds.cs
@@ -0,0 +1,63 @@
+﻿using System.Linq;
+using System.Numerics;
+using System.Threading.Tasks;
+using Uchu.Physics;
+using Uchu.World;
+using Uchu.World.Scripting.Native;
+
+namespace Uchu.StandardScripts.AvantGardens
+{
+    public class MonumentBirds : NativeScript
+    {
+        private const string Animation = "fly1";
+
+        public override Task LoadAsync()
+        {
+            foreach (var gameObject in Zone.GameObjects.Where(g => g.Lot == 14586))
+            {
+                Mount(gameObject);
+            }
+
+            Listen(Zone.OnObject, obj =>
+            {
+                if (!(obj is GameObject gameObject)) return;
+
+                if (gameObject.Lot == 14586)
+                {
+                    Mount(gameObject);
+                }
+            });
+
+            return Task.CompletedTask;
+        }
+
+        public void Mount(GameObject gameObject)
+        {
+            if (!gameObject.TryGetComponent<DestructibleComponent>(out var destructible)) return;
+
+            var physics = gameObject.AddComponent<PhysicsComponent>();
+
+            var size = Vector3.One * 2;
+
+            var physicsObject = BoxBody.Create(
+                gameObject.Zone.Simulation,
+                gameObject.Transform.Position,
+                gameObject.Transform.Rotation,
+                size
+            );
+
+            physics.SetPhysics(physicsObject);
+
+            Listen(physics.OnEnter, async other =>
+            {
+                if (!(other.GameObject is Player player)) return;
+
+                gameObject.Animate(Animation);
+
+                await Task.Delay(1000);
+
+                await destructible.SmashAsync(player, player);
+            });
+        }
+    }
+}
