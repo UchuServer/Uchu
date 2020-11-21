ef59943047238206f40a82f26b1f3edb115944ea
enteryournamehere <11255568+enteryournamehere@users.noreply.github.com>
2020-11-21 17:54:10 +0100

    Get zone path from cdclient instead of file search

--- Uchu.World/Client/ZoneParser.cs
+++ Uchu.World/Client/ZoneParser.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Xml.Serialization;
 using InfectedRose.Luz;
@@ -10,6 +11,7 @@ using InfectedRose.Triggers;
 using RakDotNet.IO;
 using Uchu.Core;
 using Uchu.Core.IO;
+using Uchu.Core.Client;
 
 namespace Uchu.World.Client
 {
@@ -33,76 +35,81 @@ namespace Uchu.World.Client
 
             Logger.Information("Parsing zone info...");
             
-            var luzFiles = _resources.GetAllFilesWithExtension("luz");
-
-            foreach (var luzFile in luzFiles)
-            {
-                Logger.Information($"Parsing: {luzFile}");
+            await using var ctx = new CdClientContext();
+            
+            var zone = ctx.ZoneTableTable.FirstOrDefault(zone => zone.ZoneID == seek);
                 
-                try
+            if (zone == default)
+            {
+                Logger.Error($"Cannot find zone {seek}");
+                return;
+            };
+
+            var luzFile = Path.Combine("maps", zone.ZoneName.ToLower());
+
+            if (!luzFile.EndsWith(".luz")) return;
+            
+            Logger.Information($"Parsing: {luzFile}");
+
+            try
+            {
+                var luz = new LuzFile();
+
+                await using var stream = _resources.GetStream(luzFile);
+
+                var reader = new BitReader(stream);
+
+                luz.Deserialize(reader);
+
+                var path = Path.GetDirectoryName(luzFile);
+
+                var lvlFiles = new List<LvlFile>();
+
+                foreach (var scene in luz.Scenes)
                 {
-                    var luz = new LuzFile();
+                    await using var sceneStream = _resources.GetStream(Path.Combine(path, scene.FileName));
 
-                    await using var stream = _resources.GetStream(luzFile);
+                    using var sceneReader = new BitReader(sceneStream);
+                        
+                    Logger.Information($"Parsing: {scene.FileName}");
+                        
+                    var lvl = new LvlFile();
 
-                    var reader = new BitReader(stream);
+                    lvl.Deserialize(sceneReader);
 
-                    luz.Deserialize(reader);
+                    lvlFiles.Add(lvl);
 
-                    if (luz.WorldId != seek) continue;
-                    
-                    var path = Path.GetDirectoryName(luzFile);
-
-                    var lvlFiles = new List<LvlFile>();
-
-                    foreach (var scene in luz.Scenes)
+                    if (lvl.LevelObjects?.Templates == default) continue;
+                        
+                    foreach (var template in lvl.LevelObjects.Templates)
                     {
-                        await using var sceneStream = _resources.GetStream(Path.Combine(path, scene.FileName));
-
-                        using var sceneReader = new BitReader(sceneStream);
-                        
-                        Logger.Information($"Parsing: {scene.FileName}");
-                        
-                        var lvl = new LvlFile();
-
-                        lvl.Deserialize(sceneReader);
-
-                        lvlFiles.Add(lvl);
-
-                        if (lvl.LevelObjects?.Templates == default) continue;
-                        
-                        foreach (var template in lvl.LevelObjects.Templates)
-                        {
-                            template.ObjectId |= 70368744177664;
-                        }
+                        template.ObjectId |= 70368744177664;
                     }
-
-                    var terrainStream = _resources.GetStream(Path.Combine(path, luz.TerrainFileName));
-
-                    using var terrainReader = new BitReader(terrainStream);
-                    
-                    var terrain = new TerrainFile();
-
-                    terrain.Deserialize(terrainReader);
-                    
-                    var triggers = await TriggerDictionary.FromDirectoryAsync(Path.Combine(_resources.RootPath, path));
-                    
-                    Logger.Information($"Parsed: {(ZoneId) luz.WorldId}");
-
-                    Zones[(int) luz.WorldId] = new ZoneInfo
-                    {
-                        LuzFile = luz,
-                        LvlFiles = lvlFiles,
-                        TriggerDictionary = triggers,
-                        TerrainFile = terrain
-                    };
-                    
-                    break;
                 }
-                catch (Exception e)
+
+                var terrainStream = _resources.GetStream(Path.Combine(path, luz.TerrainFileName));
+
+                using var terrainReader = new BitReader(terrainStream);
+                    
+                var terrain = new TerrainFile();
+
+                terrain.Deserialize(terrainReader);
+                    
+                var triggers = await TriggerDictionary.FromDirectoryAsync(Path.Combine(_resources.RootPath, path));
+                    
+                Logger.Information($"Parsed: {(ZoneId) luz.WorldId}");
+
+                Zones[(int) luz.WorldId] = new ZoneInfo
                 {
-                    Logger.Error($"Failed to parse {luzFile}: {e.Message}\n{e.StackTrace}");
-                }
+                    LuzFile = luz,
+                    LvlFiles = lvlFiles,
+                    TriggerDictionary = triggers,
+                    TerrainFile = terrain
+                };
+            }
+            catch (Exception e)
+            {
+                Logger.Error($"Failed to parse {luzFile}: {e.Message}\n{e.StackTrace}");
             }
         }
     }
