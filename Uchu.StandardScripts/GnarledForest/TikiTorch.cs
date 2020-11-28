890d678e77eef4c9c57f9f6248be04156e798103
encry <17jbradford@tqacademy.co.uk>
2020-11-28 12:48:58 +0000

    Tiki Torches

---
+++ Uchu.StandardScripts/GnarledForest/TikiTorch.cs
@@ -0,0 +1,43 @@
+﻿using System.Numerics;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Query.ExpressionTranslators.Internal;
+using Microsoft.Scripting;
+using Uchu.World;
+using Uchu.World.Scripting.Native;
+
+namespace Uchu.StandardScripts.GnarledForest
+{
+    [ZoneSpecific(1300)]
+    public class TikiTorch: NativeScript
+    {
+        private const string ScriptName = "ScriptComponent_946_script_name__removed";
+        public override Task LoadAsync()
+        {
+            foreach (var gameObject in HasLuaScript(ScriptName))
+            {
+                Listen(gameObject.OnInteract, async player =>
+                {
+                    player.Message(new PlayAnimationMessage
+                    {
+                        Associate = gameObject,
+                        AnimationsId = "interact",
+                        PlayImmediate = false,
+                        TriggerOnCompleteMessage = false,
+                    });
+                    player.Message(new ScriptNetworkVarUpdateMessage { Associate = gameObject, LDFInText = "bIsInUse=7:1" });
+
+                    for (int i = 0; i < 2; i++)
+                    {
+                        var loot = InstancingUtilities.InstantiateLoot(Lot.Imagination, player, gameObject, gameObject.Transform.Position + Vector3.UnitY);
+                        Start(loot);
+                    }
+                    
+                    await Task.Delay(500);
+                    player.Message(new ScriptNetworkVarUpdateMessage { Associate = gameObject, LDFInText = "bIsInUse=7:0" });
+                });
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Uchu.World/Packets/GameMessages/Server/ScriptNetworkVarUpdateMessage.cs
@@ -0,0 +1,23 @@
+﻿using RakDotNet.IO;
+using Uchu.Core;
+
+namespace Uchu.World
+{
+    public class ScriptNetworkVarUpdateMessage : ServerGameMessage
+    {
+        public override GameMessageId GameMessageId => GameMessageId.ScriptNetworkVarUpdate;
+
+        public string LDFInText;
+
+        public override void SerializeMessage(BitWriter writer)
+        {
+            writer.Write((uint)LDFInText.Length);
+            writer.WriteString(LDFInText, LDFInText.Length, true);
+
+            if (LDFInText.Length != 0)
+            {
+                writer.Write<short>(0);
+            }
+        }
+    }
+}
