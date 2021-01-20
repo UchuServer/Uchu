ccffa72c0a77fab99cb953e08537c50e21e58216
TheNexusAvenger <zpcook@googlemail.com>
2021-01-20 16:56:55 -0500

    Resolve code style request.

--- Uchu.World/Client/CdClient/TableCache.cs
+++ Uchu.World/Client/CdClient/TableCache.cs
@@ -13,11 +13,11 @@ namespace Uchu.World.Client
     /// </summary>
     public class TableCache<T> where T : class
     {
-        private PropertyInfo cdClientContextProperty;
-        private T[] cachedData;
-        private long lastAccessTime = 0;
-        private bool clearDataQueued = false;
-        private int clearTimeMilliseconds = 3000;
+        private PropertyInfo CdClientContextProperty;
+        private T[] CachedData;
+        private long LastAccessTime = 0;
+        private bool ClearDataQueued = false;
+        private int ClearTimeMilliseconds = 3000;
         
         /// <summary>
         /// Creates the table cache.
@@ -28,12 +28,12 @@ namespace Uchu.World.Client
             foreach (var property in typeof(CdClientContext).GetProperties())
             {
                 if (property.Name != tableName) continue;
-                cdClientContextProperty = property;
+                CdClientContextProperty = property;
                 break;
             }
             
             // Throw an exception if the table name is invalid.
-            if (cdClientContextProperty == null)
+            if (CdClientContextProperty == null)
             {
                 throw new InvalidOperationException("Table name doesn't exist: " + tableName);
             }
@@ -45,23 +45,23 @@ namespace Uchu.World.Client
         public async Task<T[]> GetValuesAsync()
         {
             // Update the last access time and fetch the data if it isn't stored.
-            lastAccessTime = DateTimeOffset.Now.ToUnixTimeSeconds();
-            if (cachedData == default)
+            LastAccessTime = DateTimeOffset.Now.ToUnixTimeSeconds();
+            if (CachedData == default)
             {
                 // Fetch the data.
                 await using var cdContext = new CdClientContext();
-                cachedData = ((DbSet<T>) cdClientContextProperty.GetValue(cdContext))?.ToArray();
+                CachedData = ((DbSet<T>) CdClientContextProperty.GetValue(cdContext))?.ToArray();
 
                 // Queue clearing the data.
-                if (!clearDataQueued)
+                if (!ClearDataQueued)
                 {
-                    clearDataQueued = true;
+                    ClearDataQueued = true;
                     Task.Run(ClearQueueTimeAsync);
                 }
             }
 
             // Return the cached data.
-            return cachedData;
+            return CachedData;
         }
 
         /// <summary>
@@ -73,13 +73,13 @@ namespace Uchu.World.Client
             while (true)
             {
                 // Wait the delay before checking to clear the table.
-                var startLastAccessTime = lastAccessTime;
-                await Task.Delay(clearTimeMilliseconds);
+                var startLastAccessTime = LastAccessTime;
+                await Task.Delay(ClearTimeMilliseconds);
 
                 // Clear the table if it hasn't been accessed recently.
-                if (startLastAccessTime != lastAccessTime) continue;
-                cachedData = default;
-                clearDataQueued = false;
+                if (startLastAccessTime != LastAccessTime) continue;
+                CachedData = default;
+                ClearDataQueued = false;
                 GC.Collect();
                 return;
             }
