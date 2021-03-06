e46df3f10ebb08624ed89cba7370c6be29332143
TheNexusAvenger <zpcook@googlemail.com>
2021-03-06 18:54:07 -0500

    Fix NullReferenceException with launchpads

--- Uchu.World/Objects/Components/ReplicaComponents/CharacterComponent.cs
+++ Uchu.World/Objects/Components/ReplicaComponents/CharacterComponent.cs
@@ -211,7 +211,7 @@ namespace Uchu.World
         public int InventorySize { get; set; }
         public int BaseImagination { get; set; }
         public int BaseHealth { get; set; }
-        public string Rocket { get; private set; }
+        public string Rocket { get; set; }
         public int LaunchedRocketFrom { get; set; }
         public long LastActivity { get; private set; }
         public bool FreeToPlay { get; private set; }
@@ -575,14 +575,22 @@ namespace Uchu.World
             writer.Write((ulong) TotalFirstPlaceFinishes);
             writer.WriteBit(false);
 
-            writer.WriteBit(LandingByRocket);
-
-            if (LandingByRocket)
+            if (Rocket != default)
             {
-                var rocketString = Rocket;
-                
-                writer.Write((ushort) rocketString.Length);
-                writer.WriteString(rocketString, rocketString.Length, true);
+                // Send the rocket if it is set to land by rocket and rocket is defined.
+                writer.WriteBit(LandingByRocket);
+
+                if (LandingByRocket)
+                {
+                    var rocketString = Rocket;
+                    writer.Write((ushort) rocketString.Length);
+                    writer.WriteString(rocketString, rocketString.Length, true);
+                }
+            }
+            else
+            {
+                // Set the landing as not by rocket. Even if it is, this prevents a null reference exception.
+                writer.WriteBit(false);
             }
 
             WritePart4(writer);
