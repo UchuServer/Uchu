7c3ec950489da5d6e87bc994269c7d72b5106e5d
TheNexusAvenger <13441476+TheNexusAvenger@users.noreply.github.com>
2021-06-11 09:52:53 -0400

    Create flag packet property.

---
+++ Uchu.Core.Test/Serializable/Structure/Property/MockPacketProperty.cs
@@ -0,0 +1,56 @@
+using System.Collections.Generic;
+using System.Reflection;
+using RakDotNet.IO;
+
+namespace Uchu.Core.Test.Serializable.Structure.Property
+{
+    public class MockPacketProperty : IPacketProperty
+    {
+        /// <summary>
+        /// Property that is read from or read to.
+        /// </summary>
+        public PropertyInfo Property { get; }
+
+        /// <summary>
+        /// Whether the mock packet property was written.
+        /// </summary>
+        public bool DataWritten { get; set; } = false;
+        
+        /// <summary>
+        /// Whether the mock packet property was read from.
+        /// </summary>
+        public bool DataRead { get; set; } = false;
+        
+        
+        /// <summary>
+        /// Creates the packet property.
+        /// </summary>
+        /// <param name="property">Property to write.</param>
+        public MockPacketProperty(PropertyInfo property)
+        {
+            this.Property = property;
+        }
+
+        /// <summary>
+        /// Writes the property to the writer.
+        /// </summary>
+        /// <param name="objectToWrite">Object with the property to write.</param>
+        /// <param name="writer">Bit writer to write to.</param>
+        /// <param name="writtenProperties">Properties that were previously written.</param>
+        public void Write(object objectToWrite, BitWriter writer, Dictionary<string, object> writtenProperties)
+        {
+            this.DataWritten = true;
+        }
+
+        /// <summary>
+        /// Reads the property to the reader.
+        /// </summary>
+        /// <param name="objectToWrite">Object with the property to read.</param>
+        /// <param name="reader">Bit reader to read to.</param>
+        /// <param name="readProperties">Properties that were previously read.</param>
+        public void Read(object objectToWrite, BitReader reader, Dictionary<string, object> readProperties)
+        {
+            this.DataRead = true;
+        }
+    }
+}
