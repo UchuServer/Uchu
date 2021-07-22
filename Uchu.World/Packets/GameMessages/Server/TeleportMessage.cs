7e587a45c6fc0a0fd2de8bca16e4cd1159c1d1bb
enteryournamehere <11255568+enteryournamehere@users.noreply.github.com>
2021-07-22 11:30:27 +0200

    Fix teleport GM rotation

--- Uchu.World/Packets/GameMessages/Server/TeleportMessage.cs
+++ Uchu.World/Packets/GameMessages/Server/TeleportMessage.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Numerics;
 using RakDotNet.IO;
 
@@ -6,8 +7,6 @@ namespace Uchu.World
     public class TeleportMessage : ServerGameMessage
     {
         public override GameMessageId GameMessageId => GameMessageId.Teleport;
-
-        public bool NoGravity { get; set; }
 
         public bool IgnoreY { get; set; } = true;
 
@@ -30,9 +29,12 @@ namespace Uchu.World
             writer.Write(Position);
 
             writer.WriteBit(UseNavMesh);
-            writer.WriteBit(NoGravity);
 
-            writer.Write(Rotation);
+            writer.WriteBit(Math.Abs(Rotation.W) > 0.01f);
+            writer.Write(Rotation.W);
+            writer.Write(Rotation.X);
+            writer.Write(Rotation.Y);
+            writer.Write(Rotation.Z);
 
         }
     }
