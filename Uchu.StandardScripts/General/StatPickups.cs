534ff421d1298bc4359fa6cba0393ff535485bd5
Fro Zen <frozenreflex1@gmail.com>
2021-08-07 18:25:18 -0500

    Use Dictionary instead of Switch in StatPickups, add logging for unimplemented scripts

--- Uchu.StandardScripts/General/StatPickups.cs
+++ Uchu.StandardScripts/General/StatPickups.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Uchu.Core.Resources;
 using Uchu.World;
@@ -9,81 +10,33 @@ namespace Uchu.StandardScripts.General
     {
         public override Task LoadAsync()
         {
+            var pickups = new Dictionary<int, int>()
+            {
+                {Lot.Imagination, 13},
+                {Lot.TwoImagination, 129},
+                {Lot.ThreeImagination, 906},
+                {Lot.FiveImagination, 907},
+                {Lot.Health, 5},
+                {Lot.TwoHealth, 902},
+                {Lot.ThreeHealth, 903},
+                {Lot.FiveHealth, 904},
+                {Lot.TenHealth, 905},
+                {Lot.Armor, 747},
+                {Lot.TwoArmor, 909},
+                {Lot.ThreeArmor, 910},
+                {Lot.FiveArmor, 911},
+                {Lot.TenArmor, 912},
+            };
             Listen(Zone.OnPlayerLoad, player =>
             {
                 Listen(player.OnLootPickup, lot =>
                 {
+                    if (!pickups.ContainsKey(lot)) return Task.CompletedTask;
                     var stats = player.GetComponent<DestroyableComponent>();
                     var skill = player.GetComponent<SkillComponent>();
                     var missionInventoryComponent = player.GetComponent<MissionInventoryComponent>();
-                    var skillExecute = 0;
-                    switch (lot)
-                    {
-                        case Lot.Imagination:
-                            //stats.Imagination += 1;
-                            skillExecute = 13;
-                            break;
-                        case Lot.TwoImagination:
-                            //stats.Imagination += 2;
-                            skillExecute = 129;
-                            break;
-                        case Lot.ThreeImagination:
-                            //stats.Imagination += 3;
-                            skillExecute = 906;
-                            break;
-                        case Lot.FiveImagination:
-                            //stats.Imagination += 5;
-                            skillExecute = 907;
-                            break;
-                        case Lot.TenImagination:
-                            //stats.Imagination += 10;
-                            skillExecute = 908;
-                            break;
-                        case Lot.Health:
-                            //stats.Health += 1;
-                            skillExecute = 5;
-                            break;
-                        case Lot.TwoHealth:
-                            //stats.Health += 2;
-                            skillExecute = 902;
-                            break;
-                        case Lot.ThreeHealth:
-                            //stats.Health += 3;
-                            skillExecute = 903;
-                            break;
-                        case Lot.FiveHealth:
-                            //stats.Health += 5;
-                            skillExecute = 904;
-                            break;
-                        case Lot.TenHealth:
-                            //stats.Health += 10;
-                            skillExecute = 905;
-                            break;
-                        case Lot.Armor:
-                            //stats.Armor += 1;
-                            skillExecute = 747;
-                            break;
-                        case Lot.TwoArmor:
-                            //stats.Armor += 2;
-                            skillExecute = 909;
-                            break;
-                        case Lot.ThreeArmor:
-                            //stats.Armor += 3;
-                            skillExecute = 910;
-                            break;
-                        case Lot.FiveArmor:
-                            //stats.Armor += 5;
-                            skillExecute = 911;
-                            break;
-                        case Lot.TenArmor:
-                            //stats.Armor += 10;
-                            skillExecute = 912;
-                            break;
-                        default:
-                            return Task.CompletedTask;
-                    }
-                    skill.CalculateSkillAsync(skillExecute, player);
-                    missionInventoryComponent.StatPickupsAsync(skillExecute);
+                    skill.CalculateSkillAsync(pickups[lot], player);
+                    missionInventoryComponent.StatPickupsAsync(pickups[lot]);
                     return Task.CompletedTask;
                 });
 
