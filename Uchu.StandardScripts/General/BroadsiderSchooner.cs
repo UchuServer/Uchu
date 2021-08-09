855d941e84f1b56efe63902239dc8bdfd8c1e9d7
Fro Zen <frozenreflex1@gmail.com>
2021-08-08 21:59:53 -0500

    Add script for Broadsider schooner ability

---
+++ Uchu.StandardScripts/General/BroadsiderSchooner.cs
@@ -0,0 +1,27 @@
+using System.Numerics;
+using System.Threading.Tasks;
+using Uchu.World;
+using Uchu.World.Scripting.Native;
+
+namespace Uchu.StandardScripts.General
+{
+    /// <summary>
+    /// Native implementation of scripts/equipmentscripts/buccaneervaliantship.lua
+    /// </summary>
+    [ScriptName("buccaneervaliantship.lua")]
+    public class BroadsiderSchooner : ObjectScript
+    {
+        public BroadsiderSchooner(GameObject gameObject) : base(gameObject)
+        {
+            Task.Run(async () => 
+            {
+                await Task.Delay(1000);
+                var skillComponent = gameObject.GetComponent<SkillComponent>();
+                skillComponent.CalculateSkillAsync(982);
+                //ForceMovementBehavior isn't finished yet, but the script works as intended.
+                await Task.Delay(1100);
+                gameObject.GetComponent<DestructibleComponent>().SmashAsync(gameObject);
+            });
+        }
+    }
+}
