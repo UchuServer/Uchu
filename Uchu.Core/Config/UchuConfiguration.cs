5b290a1dc3cff34a45d6d527a69d64df8710024b
TheNexusAvenger <13441476+TheNexusAvenger@users.noreply.github.com>
2021-09-11 03:03:54 -0400

    Disable Redis by default (no longer mentioned in README).

--- Uchu.Core/Config/UchuConfiguration.cs
+++ Uchu.Core/Config/UchuConfiguration.cs
@@ -142,7 +142,7 @@ namespace Uchu.Core.Config
         /// if no service is installed and the connection will
         /// always timeout.
         /// </summary>
-        [XmlElement] public bool UseService { get; set; } = true;
+        [XmlElement] public bool UseService { get; set; } = false;
         
         /// <summary>
         /// Hostname to use when connecting to the cache service
