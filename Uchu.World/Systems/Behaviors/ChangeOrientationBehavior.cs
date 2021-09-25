12c3a625c24545aa965924668fc9700a52acc9aa
Fro Zen <frozenreflex1@gmail.com>
2021-09-25 17:37:16 -0500

    Ignore variables in ChangeOrientationBehavior

--- Uchu.World/Systems/Behaviors/ChangeOrientationBehavior.cs
+++ Uchu.World/Systems/Behaviors/ChangeOrientationBehavior.cs
@@ -4,12 +4,12 @@ namespace Uchu.World.Systems.Behaviors
 {
     public class ChangeOrientationBehavior : BehaviorBase
     {
-        //private float Angle { get; set; }
-        //private float Duration { get; set; }
+        private float Angle { get; set; }
+        private float Duration { get; set; }
         private bool OrientCaster { get; set; }
-        //private bool Relative { get; set; }
-        //private bool ToAngle { get; set; }
-        //private bool ToPoint { get; set; }
+        private bool Relative { get; set; }
+        private bool ToAngle { get; set; }
+        private bool ToPoint { get; set; }
         private bool ToTarget { get; set; }
         public override BehaviorTemplateId Id => BehaviorTemplateId.ChangeOrientation;
         public override async Task BuildAsync()
@@ -20,6 +20,9 @@ namespace Uchu.World.Systems.Behaviors
 
         public override void ExecuteStart(BehaviorExecutionParameters parameters)
         {
+            parameters.NpcContext.Associate.Transform.LookAt(parameters.BranchContext.Target.Transform.Position);
+            //the variables seem to do nothing
+            /*
             if (OrientCaster)
             {
                 if (ToTarget)
@@ -27,6 +30,14 @@ namespace Uchu.World.Systems.Behaviors
                     parameters.NpcContext.Associate.Transform.LookAt(parameters.BranchContext.Target.Transform.Position);
                 }
             }
+            else
+            {
+                if (ToTarget)
+                {
+                    parameters.NpcContext.Associate.Transform.LookAt(parameters.BranchContext.Target.Transform.Position);
+                }
+            }
+            */
         }
     }
 }
