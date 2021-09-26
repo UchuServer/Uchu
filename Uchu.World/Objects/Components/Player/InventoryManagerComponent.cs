c22808cf11475a4ab85e65b5b360faf33587dae7
TecCheck <mcteccy@protonmail.com>
2021-09-26 13:31:24 +0200

    Allow for preserving object ID when moving item

--- Uchu.World/Objects/Components/Player/InventoryManagerComponent.cs
+++ Uchu.World/Objects/Components/Player/InventoryManagerComponent.cs
@@ -331,10 +331,11 @@ namespace Uchu.World
         /// <param name="rootItem">An optional parent item</param>
         /// <param name="lootType">The reason this lot is given to the player</param>
         /// <param name="showFlyingLoot">Whether to show flying loot animation</param>
+        /// <param name="objectId">The objectId that the moved item should have if it is moved as a whole stack</param>
         /// <returns>The list of items that were created while adding the lot</returns>
         public async Task AddLotAsync(Lot lot, uint count, LegoDataDictionary settings = default,
             InventoryType inventoryType = default, Item rootItem = default, LootType lootType = default,
-            bool showFlyingLoot = true)
+            bool showFlyingLoot = true, ObjectId objectId = default)
         {
             var createdItems = new List<Item>();
             
@@ -394,7 +395,7 @@ namespace Uchu.World
                 while (totalToAdd > 0)
                 {
                     var toAdd = (uint) Min(stackSize, (int) totalToAdd);
-                    var item = await Item.Instantiate(GameObject, lot, inventory, toAdd, extraInfo: settings, rootItem: rootItem);
+                    var item = await Item.Instantiate(GameObject, lot, inventory, toAdd, extraInfo: settings, rootItem: rootItem, objectId: objectId);
 
                     totalToAdd -= toAdd;
                     totalAdded += toAdd;
@@ -609,14 +610,15 @@ namespace Uchu.World
         /// <param name="destination">The destination inventory to move to</param>
         /// <param name="silent">Whether to send inventory update messages or not</param>
         /// <param name="showFlyingLoot">Whether to show flying loot animation</param>
+        /// <param name="objectId">The objectId that the moved item should have if it's moved as a whole stack</param>
         public async Task MoveItemBetweenInventoriesAsync(Item item, uint count, InventoryType source,
-            InventoryType destination, bool silent = false, bool showFlyingLoot = false)
+            InventoryType destination, bool silent = false, bool showFlyingLoot = false, ObjectId objectId = default)
         {
             if (item == null)
                 return;
 
             await RemoveItemAsync(item, count, source, silent);
-            await AddLotAsync(item.Lot , count, item.Settings, destination, lootType: LootType.Inventory, showFlyingLoot: showFlyingLoot); // TODO: find out if lootType is correct (what's LootType.Relocate?)
+            await AddLotAsync(item.Lot , count, item.Settings, destination, lootType: LootType.Inventory, showFlyingLoot: showFlyingLoot, objectId: objectId); // TODO: find out if lootType is correct (what's LootType.Relocate?)
         }
         
         #endregion methods
