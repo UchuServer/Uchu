dba75ac6e6300d00bd0061233a8ce912c0a3b2bf
Teccheck <mcteccy@protonmail.com>
2023-09-01 01:37:52 +0200

    Clean up BuildBorderComponent

--- Uchu.World/Objects/Components/ReplicaComponents/BuildBorderComponent.cs
+++ Uchu.World/Objects/Components/ReplicaComponents/BuildBorderComponent.cs
@@ -10,34 +10,13 @@ namespace Uchu.World
 
         protected BuildBorderComponent()
         {
-            Logger.Debug("New BuildBorderComponent");
             Listen(OnStart, () => Listen(GameObject.OnInteract, OnInteract));
         }
 
-        private async void OnInteract(Player player)
+        private void OnInteract(Player player)
         {
-            Logger.Debug($"{player} interacted with BuildBorderComponent on {GameObject}");
-
-            // Maybe this is usefull. It's no implemented yet
             var inventory = player.GetComponent<InventoryComponent>();
             inventory.PushEquippedItemState();
-
-            return;
-
-            // This is not needed
-            var inventoryManager = player.GetComponent<InventoryManagerComponent>();
-            var thinkingHat = inventoryManager.FindItem(Lot.ThinkingHat);
-            await thinkingHat.EquipAsync(true);
-
-            // This is in DLU but seems to mess things up
-            player.Message(new StartArrangingWithItemMessage
-            {
-                Associate = player,
-                FirstTime = true,
-                BuildArea = GameObject,
-                StartPosition = player.Transform.Position
-            });
-
         }
 
         public override void Construct(BitWriter writer)
